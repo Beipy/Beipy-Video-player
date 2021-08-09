e8228948b4031596f7f4e49642119aead09a4351
yons <yons@yonsdeiMac-Pro.local>
2021-08-09 18:26:28 +0800

    #1 配置webpack编译打包

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @*      ____       _             :  ******************************
+ * @*     / __ )___  (_)___  __  __ :  Author:Beipy
+ * @*    / __  / _ \/ / __ \/ / / / :  WebURL:http://www.beipy.com
+ * @*   / /_/ /  __/ / /_/ / /_/ /  :  Github:http://github.com/beipy
+ * @*  /_____/\___/_/ .___/\__, /   :  别有梓人传，精艺夺天工。便使玉人雕琢，妙手略相同。
+ * @*              /_/    /____/    :  ******************************
+ * @Descripttion: 
+ * @version: 
+ * @Date: 2021-08-09 17:42:06
+ * @LastEditors: Beipy
+ * @FilePath: /Beipy-Video-player/src/index.js
+ * @LastEditTime: 2021-08-09 18:10:56
+ */
+// 
+import './css/beipy-video.scss'
+
+const test = () => {
+  console.log('asdfjklasdfsl; ');
+}
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+/*
+ * @*      ____       _             :  ******************************
+ * @*     / __ )___  (_)___  __  __ :  Author:Beipy
+ * @*    / __  / _ \/ / __ \/ / / / :  WebURL:http://www.beipy.com
+ * @*   / /_/ /  __/ / /_/ / /_/ /  :  Github:http://github.com/beipy
+ * @*  /_____/\___/_/ .___/\__, /   :  别有梓人传，精艺夺天工。便使玉人雕琢，妙手略相同。
+ * @*              /_/    /____/    :  ******************************
+ * @Descripttion: 
+ * @version: 
+ * @Date: 2021-08-09 17:32:09
+ * @LastEditors: Beipy
+ * @FilePath: /Beipy-Video-player/webpack.config.js
+ * @LastEditTime: 2021-08-09 18:20:31
+ */
+// resolve用来拼接绝对路径的方法
+const { resolve } = require('path')
+
+module.exports = {
+  mode: "development",
+  
+  module: {
+    rules: [
+       {
+        // 匹配哪些文件
+        test: /\.scss$/,
+        // 使用哪些loader进行处理
+        use: [
+          // use数组中loader执行顺序：从右到左，从下到上，依次执行(先执行css-loader)
+          // style-loader：创建style标签，将js中的样式资源插入进去，添加到head中生效
+          'style-loader',
+          // css-loader：将css文件变成commonjs模块加载到js中，里面内容是样式字符串
+          'css-loader',
+          // scss-loader：将scss文件编译成css文件，需要下载scss-loader和scss
+          'scss-loader'
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+       {
+        // url-loader：处理图片资源，问题：默认处理不了html中的img图片
+        test: /\.(jpg|png|gif)$/,
+        // 需要下载 url-loader file-loader
+        loader: 'url-loader',
+        options: {
+          // 图片大小小于8kb，就会被base64处理，优点：减少请求数量（减轻服务器压力），缺点：图片体积会更大（文件请求速度更慢）
+          // base64在客户端本地解码所以会减少服务器压力，如果图片过大还采用base64编码会导致cpu调用率上升，网页加载时变卡
+          limit: 8 * 1024,
+          // 给图片重命名，[hash:10]：取图片的hash的前10位，[ext]：取文件原来扩展名
+          name: '[hash:10].[ext]',
+          // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是conmonjs，解析时会出问题：[object Module]
+          // 解决：关闭url-loader的es6模块化，使用commonjs解析
+          esModule: false,
+          outputPath: 'imgs',
+         },
+        
+      },
+       {
+        test: /\.html$/,
+        // 处理html文件的img图片（负责引入img，从而能被url-loader进行处理）
+        loader: 'html-loader',
+      },
+       // 打包其他资源(除了html/js/css资源以外的资源)
+      {
+        // 排除html|js|css|less|jpg|png|gif文件
+        exclude: /\.(html|js|css|less|jpg|png|gif)/,
+        // file-loader：处理其他文件
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          outputPath: 'media',
+        },
+      },
+    ]
+  },
+   devServer: {
+    // 项目构建后路径
+    contentBase: resolve(__dirname, 'build'),
+    // 启动gzip压缩
+    compress: true,
+    // 端口号
+    port: 6000,
+    // 自动打开浏览器
+    open: true,
+  },
+}
