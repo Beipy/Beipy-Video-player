ff1a7b350d5ccca02e91e0ebe45c3fffd68db624
Tach <tach@TachdeiMac.local>
2021-08-20 15:12:00 +0800

    #1 配置Icon.js文件

--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@
  * @*   / /_/ /  __/ / /_/ / /_/ /  :  Github:http://github.com/beipy
  * @*  /_____/\___/_/ .___/\__, /   :  别有梓人传，精艺夺天工。便使玉人雕琢，妙手略相同。
  * @*              /_/    /____/    :  ******************************
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Date: 2021-08-09 17:42:06
  * @LastEditors: Beipy
  * @FilePath: /Beipy-Video-player/src/index.js
- * @LastEditTime: 2021-08-11 10:51:22
+ * @LastEditTime: 2021-08-20 12:24:29
  */
-// 
-import './css/beipy-video.scss'
+//
+import "./css/beipy-video.scss";
+import VideoIcon from "./js/icon";
 
 const test = () => {
-  console.log('asdfjklasdfsl; ');
-}
-test()
+  console.log("执行Icon渲染 ", VideoIcon);
+
+  let data = "";
+  for (const key in VideoIcon) {
+    data =
+      data +
+      '<div style="text-align: center; padding: 10px;display: inline-block;"> <div>' +
+      VideoIcon[key].InitHtml +
+      "</div> <p>" +
+      VideoIcon[key].desc +
+      "</p> <p>" +
+      VideoIcon[key].key +
+      "</p></div>";
+  }
+  document.getElementById("box").innerHTML = data;
+};
+test();
